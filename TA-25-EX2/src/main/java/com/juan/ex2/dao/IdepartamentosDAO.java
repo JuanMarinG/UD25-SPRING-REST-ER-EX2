7a80a9448bb39a8103b1135c18e6eb5e0b2a5c05
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-08-18 13:18:04 +0200

    Create IdepartamentosDAO.java

---
+++ TA-25-EX2/src/main/java/com/juan/ex2/dao/IdepartamentosDAO.java
@@ -0,0 +1,7 @@
+package com.juan.ex2.dao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.juan.ex2.dto.Departamentos;
+
+public interface IdepartamentosDAO extends JpaRepository<Departamentos, Long>{
+
+}
