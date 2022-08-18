4b9e41c741c126ab3e3120bac5e986e4ceaf55ab
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-08-18 18:40:45 +0200

    Create IempleadosService.java

---
+++ TA-25-EX2/src/main/java/com/juan/ex2/service/IempleadosService.java
@@ -0,0 +1,22 @@
+package com.juan.ex2.service;
+
+import java.util.List;
+import com.juan.ex2.dto.Empleados;
+
+public interface IempleadosService {
+
+	//ALL
+	public List <Empleados> listarEmpleados();
+	
+	//UPDATE
+	public Empleados actualizarEmpleados (Empleados empleados);
+	
+	//CREATE
+	public Empleados guardarEmpleados (Empleados empleados);
+	
+	//DELETE
+	public Empleados eliminarEmpleado (Long Id);
+	
+	//READ
+	public Empleados empleadosPorId (Long Id);
+}
