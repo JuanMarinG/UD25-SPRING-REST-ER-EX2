dc8e7f2f16ff9edc57293208b1465b85fc65ea58
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-08-18 13:18:19 +0200

    Create IempleadosDAO.java

---
+++ TA-25-EX2/src/main/java/com/juan/ex2/dao/IempleadosDAO.java
@@ -0,0 +1,6 @@
+package com.juan.ex2.dao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.juan.ex2.dto.Empleados;
+public interface IempleadosDAO extends JpaRepository <Empleados, Long>{
+
+}
