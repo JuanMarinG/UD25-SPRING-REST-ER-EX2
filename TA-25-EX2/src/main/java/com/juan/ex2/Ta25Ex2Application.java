0dbead333fe89ec997d9a37576d851eb16a51f72
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-08-18 12:28:05 +0200

    Create-Proyect-Maven

---
+++ TA-25-EX2/src/main/java/com/juan/ex2/Ta25Ex2Application.java
@@ -0,0 +1,13 @@
+package com.juan.ex2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ta25Ex2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ta25Ex2Application.class, args);
+	}
+
+}
---
+++ TA-25-EX2/src/test/java/com/juan/ex2/Ta25Ex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ta25Ex2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
