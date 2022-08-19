5012c0825c04f5f8a3aacb9ab39f2c4e8237324e
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-08-19 20:28:33 +0200

    Update Empleados.java

--- TA-25-EX2/src/main/java/com/juan/ex2/dto/Empleados.java
+++ TA-25-EX2/src/main/java/com/juan/ex2/dto/Empleados.java
@@ -19,9 +19,48 @@ public class Empleados {
 	private String DNI;
 	private String Nombre;
 	private String Apellidos;
-	private int Departamento;
 	
 	@ManyToOne
 	@JoinColumn(name = "Codigo")
 	private Departamentos departamentos;
+
+	public String getDNI() {
+		return DNI;
+	}
+
+	public void setDNI(String dNI) {
+		DNI = dNI;
+	}
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return Apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		Apellidos = apellidos;
+	}
+
+	public Departamentos getDepartamentos() {
+		return departamentos;
+	}
+
+	public void setDepartamentos(Departamentos departamentos) {
+		this.departamentos = departamentos;
+	}
+
+	@Override
+	public String toString() {
+		return "Empleados [DNI=" + DNI + ", Nombre=" + Nombre + ", Apellidos=" + Apellidos + ", departamentos="
+				+ departamentos + "]";
+	}
+	
+	
 }
