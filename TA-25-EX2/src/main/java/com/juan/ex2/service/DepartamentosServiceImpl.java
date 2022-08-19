9fac4c04e84e82897f820e1faa87defce5f8f007
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-08-20 00:40:27 +0200

    Update DepartamentosServiceImpl.java

--- TA-25-EX2/src/main/java/com/juan/ex2/service/DepartamentosServiceImpl.java
+++ TA-25-EX2/src/main/java/com/juan/ex2/service/DepartamentosServiceImpl.java
@@ -2,6 +2,8 @@ package com.juan.ex2.service;
 
 import java.util.List;
 
+import javax.persistence.Id;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,35 +12,40 @@ import com.juan.ex2.dto.Departamentos;
 
 @Service
 public class DepartamentosServiceImpl  implements IdepartamentosService{
-	
+
 	@Autowired
 	IdepartamentosDAO idepartamentosDAO;
 	
-	public List<Departamentos> listarDepartamentos(){
+	
+	@Override
+	public List<Departamentos> listarDepartamentos() {
 		return idepartamentosDAO.findAll();
 	}
 
 	@Override
 	public Departamentos actualizarDepartamento(Departamentos departamentos) {
+		
 		return idepartamentosDAO.save(departamentos);
 	}
 
 	@Override
 	public Departamentos guardarDepartamentos(Departamentos departamentos) {
-	
 		return idepartamentosDAO.save(departamentos);
 	}
 
 	@Override
-	public void  eliminarDepartamento(Long Id) {
-		idepartamentosDAO.deleteById(Id);;
+	public void eliminarDepartamento(Long Id) {
+		idepartamentosDAO.deleteById(Id);
+		
 	}
 
 	@Override
 	public Departamentos departamentosPorId(Long Id) {
-		return idepartamentosDAO.findById(Id).get();
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-	
+
+
 	
 }
