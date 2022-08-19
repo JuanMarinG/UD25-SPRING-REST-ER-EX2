8ab181dafc7cd3d3646dbbdf53270cdd5f40ec34
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-08-19 20:28:48 +0200

    Update Departamentos.java

--- TA-25-EX2/src/main/java/com/juan/ex2/dto/Departamentos.java
+++ TA-25-EX2/src/main/java/com/juan/ex2/dto/Departamentos.java
@@ -25,6 +25,53 @@ public class Departamentos {
 		@OneToMany
 		@JoinColumn(name = "Departamento")
 		private Empleados empleados;
+
+
+		public int getCodigo() {
+			return Codigo;
+		}
+
+
+		public void setCodigo(int codigo) {
+			Codigo = codigo;
+		}
+
+
+		public String getNombre() {
+			return Nombre;
+		}
+
+
+		public void setNombre(String nombre) {
+			Nombre = nombre;
+		}
+
+
+		public int getPresupuesto() {
+			return Presupuesto;
+		}
+
+
+		public void setPresupuesto(int presupuesto) {
+			Presupuesto = presupuesto;
+		}
+
+
+		public Empleados getEmpleados() {
+			return empleados;
+		}
+
+
+		public void setEmpleados(Empleados empleados) {
+			this.empleados = empleados;
+		}
+
+
+		@Override
+		public String toString() {
+			return "Departamentos [Codigo=" + Codigo + ", Nombre=" + Nombre + ", Presupuesto=" + Presupuesto
+					+ ", empleados=" + empleados + "]";
+		}
 		
 		
 	
