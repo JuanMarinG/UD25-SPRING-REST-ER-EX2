3184d718c06439d34b65ef39980bfab882400df2
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-08-20 00:40:17 +0200

    Update IdepartamentosDAO.java

--- TA-25-EX2/src/main/java/com/juan/ex2/dao/IdepartamentosDAO.java
+++ TA-25-EX2/src/main/java/com/juan/ex2/dao/IdepartamentosDAO.java
@@ -1,7 +1,12 @@
 package com.juan.ex2.dao;
+import java.util.Optional;
+
+import javax.persistence.Id;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.juan.ex2.dto.Departamentos;
 
 public interface IdepartamentosDAO extends JpaRepository<Departamentos, Long>{
+
 
 }
