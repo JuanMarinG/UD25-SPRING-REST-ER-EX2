544c44676a07dc54622b9838c2ffaff88fac9344
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-08-19 20:28:15 +0200

    Create EmpleadosServiceImpl.java

---
+++ TA-25-EX2/src/main/java/com/juan/ex2/service/EmpleadosServiceImpl.java
@@ -0,0 +1,48 @@
+package com.juan.ex2.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.juan.ex2.dao.IempleadosDAO;
+import com.juan.ex2.dto.Empleados;
+
+@Service
+public class EmpleadosServiceImpl implements IempleadosService{
+
+	@Autowired
+	IempleadosDAO iempleadosDAO;
+
+	@Override
+	public List<Empleados> listarEmpleados() {
+		// TODO Auto-generated method stub
+		return iempleadosDAO.findAll();
+	}
+
+	@Override
+	public Empleados actualizarEmpleados(Empleados empleados) {
+		// TODO Auto-generated method stub
+		return iempleadosDAO.save(empleados);
+	}
+
+	@Override
+	public Empleados guardarEmpleados(Empleados empleados) {
+		// TODO Auto-generated method stub
+		return iempleadosDAO.save(empleados);
+	}
+
+	@Override
+	public void eliminarEmpleado(Long Id) {
+		// TODO Auto-generated method stub
+		iempleadosDAO.deleteById(Id);;
+	}
+
+	@Override
+	public Empleados empleadosPorId(Long Id) {
+		// TODO Auto-generated method stub
+		return iempleadosDAO.findById(Id).get();
+	}
+	
+	
+}
